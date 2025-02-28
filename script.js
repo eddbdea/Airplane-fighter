65d29696e751da4f4502c16f8b48fd05152eda12
Eduard Badea <eduardbadea@outlook.com>
2025-02-28 18:35:14 +0200

    Added asteroids and managed to inserts random asteroids on a random position

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const gameBoard = [];
 let minIndex = 0;
 let maxIdnex = 4;
 let planeCol = 2;
+const MAX_ASTEROIDS = 2;
+let colIndex = 0;
+let rowIndex = 0;
 
-//create game box
+//create game box matrix (2D array)
 function createGameBoard() {
     for (let row = 0; row < MAX_ELEMENTS; ++row) {
         gameBoard[row] = []; //creates an empty array;
@@ -34,4 +37,19 @@ function movePlane(button) {
         gameBoard[2][planeCol].removeAttribute('id');
         gameBoard[2][++planeCol].id = 'plane';
     }
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * MAX_ELEMENTS); 
+}
+
+function createObstacle() {
+    colIndex = randomNumber();
+    rowIndex = 0;
+    gameBoard[rowIndex][colIndex].id = 'obstacle';
+}
+
+function moveObstacles() {
+    gameBoard[rowIndex][colIndex].removeAttribute('id');
+    gameBoard[++rowIndex][colIndex].id = 'obstacle';
 }
