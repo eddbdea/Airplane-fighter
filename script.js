0ef89f365c582ce1ef845d984f6a27326b58425e
Eduard Badea <eduardbadea@outlook.com>
2025-03-01 19:04:43 +0200

    Infinite objects, score update, backround image and objects moving on the column by itself

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const MAX_ELEMENTS = 5;
 const gameBoard = [];
-let minIndex = 0;
-let maxIdnex = 4;
-let planeCol = 2;
 const MAX_ASTEROIDS = 2;
+let planeCol = 2;
 let colIndex = 0;
 let rowIndex = 0;
+let score = 0;
 
 //create game box matrix (2D array)
 function createGameBoard() {
@@ -31,11 +30,11 @@ createGameBoard();
 
 function movePlane(button) {
     if (button.id === 'left' && planeCol > 0) {
-        gameBoard[2][planeCol].removeAttribute('id');
-        gameBoard[2][--planeCol].id = 'plane';
-    } else if (button.id === 'right' && planeCol < 4) {
-        gameBoard[2][planeCol].removeAttribute('id');
-        gameBoard[2][++planeCol].id = 'plane';
+        gameBoard[MAX_ELEMENTS - 1][planeCol].removeAttribute('id');
+        gameBoard[MAX_ELEMENTS - 1][--planeCol].id = 'plane';
+    } else if (button.id === 'right' && planeCol < MAX_ELEMENTS - 1) {
+        gameBoard[MAX_ELEMENTS - 1][planeCol].removeAttribute('id');
+        gameBoard[MAX_ELEMENTS - 1][++planeCol].id = 'plane';
     }
 }
 
@@ -47,11 +46,59 @@ function createObstacle() {
     colIndex = randomNumber();
     rowIndex = 0;
     gameBoard[rowIndex][colIndex].id = 'obstacle';
+    scoreUpdate();
 }
 
 function moveObstacles() {
+    const status = checkGameStatus(colIndex);
     gameBoard[rowIndex][colIndex].removeAttribute('id');
-    gameBoard[++rowIndex][colIndex].id = 'obstacle';
+    if (rowIndex < MAX_ELEMENTS - 1 && status === true) {
+        gameBoard[++rowIndex][colIndex].id = 'obstacle';
+    }
 }
 
-createObstacle();
+function checkLastRow() {
+    for (let col = 0; col < MAX_ELEMENTS; ++col) {
+        if (gameBoard[MAX_ELEMENTS - 1][col].id === 'obstacle') {
+            gameBoard[MAX_ELEMENTS - 1][col].removeAttribute('id');
+        }
+    }
+}
+
+function checkGameStatus(col) {
+    if (gameBoard[MAX_ELEMENTS - 1][col].id === 'plane' && 
+        gameBoard[MAX_ELEMENTS - 2][col].id === 'obstacle') {
+        finalScore();
+        clearInterval(obstacleCreate);
+        clearInterval(moveObstacle);
+        clearInterval(lastRow);
+        restartGameButton();
+        return false;
+    }
+    return true;
+}
+
+function finalScore() {
+    const finalScore = document.createElement('h2');
+    finalScore.innerText = 'You lost! Final score: ' + score;
+    document.getElementById('final-score').appendChild(finalScore);
+}
+
+function scoreUpdate() {
+    ++score;
+    document.getElementById('score').innerText = 'SCORE: ' + score;
+}
+
+function restartGameButton() {
+    const playAgain = document.createElement('button');
+    playAgain.classList.add('btn', 'btn-danger');
+    playAgain.addEventListener('click', function(e) {
+        location.reload();
+    })
+    playAgain.innerText = 'Play again!';
+    document.getElementById('restart-button').appendChild(playAgain);
+}
+
+let obstacleCreate = setInterval(createObstacle, 1000);
+let moveObstacle = setInterval(moveObstacles, 200);
+let lastRow = setInterval(checkLastRow, 500);
